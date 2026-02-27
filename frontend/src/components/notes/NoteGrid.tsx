54315b664b3cb1a96131d6e69a3c1a0db5f219c7
Ranjeet <ranjeeturd@gmail.com>
2026-02-27 12:34:39 +0530

    add notes grid

---
+++ frontend/src/components/notes/NoteGrid.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { INote } from "@/types/notes.type";
+import NoteCard from "./NoteCard";
+
+interface NotesGridProps {
+    notes: INote[];
+    onEdit: (note: INote) => void;
+}
+
+export default function NotesGrid({ notes, onEdit }: NotesGridProps) {
+    return (
+        <div
+            className="
+      grid 
+      grid-cols-1 
+      gap-6
+      sm:grid-cols-2 
+      lg:grid-cols-3 
+      xl:grid-cols-4
+    "
+        >
+            {notes.map((note, index) => (
+                <NoteCard
+                    key={note.id}
+                    note={note}
+                    onEdit={onEdit}
+                    animationDelay={index * 60}
+                />
+            ))}
+        </div>
+    );
+}
