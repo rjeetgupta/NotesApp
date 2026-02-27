017c401deb770594d4003f8840190d67f56ae351
Ranjeet <ranjeeturd@gmail.com>
2026-02-27 11:40:37 +0530

    setup redux toolkit

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios";
+import { IApiError } from "@/types/api.types";
+
+class AxiosClient {
+  private static instance: AxiosClient;
+  public readonly axiosInstance: AxiosInstance;
+
+  private constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8000/api",
+      timeout: 10000,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    this.registerRequestInterceptor();
+    this.registerResponseInterceptor();
+  }
+
+  public static getInstance(): AxiosClient {
+    if (!AxiosClient.instance) {
+      AxiosClient.instance = new AxiosClient();
+    }
+    return AxiosClient.instance;
+  }
+
+  private registerRequestInterceptor(): void {
+    this.axiosInstance.interceptors.request.use(
+      (config: any) => {
+        // In future: attach auth token here
+        // const token = localStorage.getItem("accessToken");
+        // if (token) config.headers.Authorization = `Bearer ${token}`;
+        return config;
+      },
+      (error: AxiosError) => Promise.reject(error)
+    );
+  }
+
+  private registerResponseInterceptor(): void {
+    this.axiosInstance.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      (error: AxiosError<IApiError>) => {
+        // In future: handle 401 → redirect to login
+        // if (error.response?.status === 401) { ... }
+        return Promise.reject(error);
+      }
+    );
+  }
+}
+
+export const axiosInstance = AxiosClient.getInstance().axiosInstance;
